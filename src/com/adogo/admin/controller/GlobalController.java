b31469820c88bb1163495484a205b96637b71db2
Athens Zhang <athens314@hotmail.com>
2016-03-10 10:47:45 -0500

    cleaned sidebar options

--- src/com/adogo/admin/controller/GlobalController.java
+++ src/com/adogo/admin/controller/GlobalController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GlobalController {
+	@RequestMapping("/")
+	public String gotoIndex2(){
+		return "index";
+	}
+	
 	@RequestMapping("/index")
 	public String gotoIndex(){
 		return "index";
 	}
 	
-	@RequestMapping("/")
-	public String gotoIndex2(){
-		return "index";
+	@RequestMapping("/dashboard")
+	public String gotoDashboard(){
+		return "index_dashboard";
 	}
+	
 }
