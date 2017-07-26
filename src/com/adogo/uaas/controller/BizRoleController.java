783b3172718c54a0d01597b25db32a1a7b43c773
Athens Zhang <athens314@hotmail.com>
2017-07-26 16:47:57 -0400

    added biz role views

--- src/com/adogo/uaas/controller/BizRoleController.java
+++ src/com/adogo/uaas/controller/BizRoleController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/uaas/advertiser")
-public class AdvertiserController {
+@RequestMapping("/uaas")
+public class BizRoleController {
 	
-	@RequestMapping("/apply")
+	@RequestMapping("/bizrole")
 	public String applyAdvertiser(){
-		String viewName = "uaas/advertiser_apply";
+		String viewName = "uaas/bizrole_home";
 		return viewName;
 	}
 }
