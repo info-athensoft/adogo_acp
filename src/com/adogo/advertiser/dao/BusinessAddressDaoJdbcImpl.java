d70e87fdc9e2a7c47251d8f516eefb78ee9c501f
athens <athens314@hotmail.com>
2017-12-18 22:09:11 -0500

    debugged businessAddress

--- src/com/adogo/advertiser/dao/BusinessAddressDaoJdbcImpl.java
+++ src/com/adogo/advertiser/dao/BusinessAddressDaoJdbcImpl.java
@@ -2,10 +2,15 @@ package com.adogo.advertiser.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import com.adogo.advertiser.entity.Address;
 
 import test.shared.query.v2.Query;
 
+@Component
+@Qualifier("businessAddressDaoJdbcImpl")
 public class BusinessAddressDaoJdbcImpl implements BusinessAddressDao {
 
 	@Override
