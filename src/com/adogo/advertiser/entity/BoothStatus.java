04da50970911d8de3c3312f8c978fa400165b566
Athens <athens314@hotmail.com>
2017-12-31 11:15:54 -0500

    modified BoothStatus

--- src/com/adogo/advertiser/entity/BoothStatus.java
+++ src/com/adogo/advertiser/entity/BoothStatus.java
@@ -1,9 +1,11 @@
 package com.adogo.advertiser.entity;
 
 public class BoothStatus {
-	public static final int CREATED = 0;
-	public static final int PUBLISHED = 1;
-	public static final int PENDING = 2;
-	public static final int EXPIRED = 3;
-	public static final int BANNED = 4;
+	public static final int CREATED 	= 1;
+	public static final int PUBLISHED 	= 2;
+	public static final int EXPIRED 	= 3;
+	public static final int DISABLED 	= 4;
+	public static final int TRAHSED 	= 5;
+	public static final int SUSPENED 	= 6;	
+	public static final int BANNED 		= 7;
 }
