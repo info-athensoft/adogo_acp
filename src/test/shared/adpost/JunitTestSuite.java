68bb49486dd7918fff55b0ea69b2d5049761e250
athens <athens314@hotmail.com>
2018-01-29 19:32:16 -0500

    tried junit 4

---
+++ src/test/shared/adpost/JunitTestSuite.java
@@ -0,0 +1,14 @@
+package test.shared.adpost;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ 
+	TestAdPost2.class, TestAdPost3.class
+})
+
+public class JunitTestSuite {
+}
